e9d71edaa7d09a17bf8aa04376f83133df893914
luisandra abreu duran <abreuluisandra@gmail.com>
2020-06-05 22:09:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,5 @@
 # code your #position_taken? method here!
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
 
 
-def input_to_index(input)
-  input.to -1
-end
+def position_taken?(board,index)
